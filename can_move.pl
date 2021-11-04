402569e170bafc974049a24cd8515838b255e096
kikeXD <kike@gamil.com>
2021-11-04 19:09:40 -0400

    adding true in InGame param to hive.pl and can_move.pl

--- can_move.pl
+++ can_move.pl
@@ -3,5 +3,5 @@ cell(1, 1, 1, 1, 1, 1).
 cell(1, 1, 1, 1, 2, 1).
 
 
-can_move(cell(_, Row, Column, _, StackPosition, _)) :-
-    not((cell(_, Row, Column, _, SP, _), SP > StackPosition)).
+can_move(cell(_, Row, Column, _, StackPosition, true)) :-
+    not((cell(_, Row, Column, _, SP, true), SP > StackPosition)).
