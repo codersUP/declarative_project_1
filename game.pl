3aa9e4ea3a3a7a63bebdff54ce6f02bd2215638d
kikeXD <kike@gamil.com>
2021-11-24 00:04:38 -0500

    implementing real valid moves

--- game.pl
+++ game.pl
@@ -1,3 +1,10 @@
 :- module(game, [cell/6]).
 
-cell(_, 0, 0, _, _, false).
+cell(_, 0, 0, _, 0, true).
+cell(_, 0, 1, _, 0, true).
+cell(ant, 0, 2, _, 0, true).
+cell(ant, 0, 3, _, 0, true).
+cell(ant, -1, 2, _, 0, true).
+cell(ant, -1, 3, _, 0, true).
+
+:- dynamic cell/6.
