6bd4e75e1bc3c130b7ae1c5e21c1b92e224b8651
kikeXD <kike@gamil.com>
2021-11-30 00:58:37 -0500

    select cell to put

--- bug_can_put.pl
+++ bug_can_put.pl
@@ -1,4 +1,4 @@
-:- module(bug_can_put, [bug_can_put/3]).
+:- module(bug_can_put, [bug_can_put/3, select_cell_to_put/3]).
 :- use_module(game).
 
 bug_can_put(Color, Turn, [queen]) :-
@@ -8,3 +8,13 @@ bug_can_put(Color, Turn, [queen]) :-
 bug_can_put(Color, _, BugCanPut) :-
     findall(B, cell(B, _, _, Color, _, false), BugCanPut1),
     sort(BugCanPut1, BugCanPut).
+
+
+select_cell_to_put(Color, Bug, Cell) :-
+    findall(SP, cell(Bug, 0, 0, Color, SP, false), SPs),
+    sort(SPs, SPs_sorted),
+    get_first_or_0(SPs_sorted, SP_lower),
+    Cell = [Bug, 0, 0, Color, SP_lower].
+
+get_first_or_0([], 0).
+get_first_or_0([LH|_], LH).
