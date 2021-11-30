737c3b8015b59933caccf4fdff66f1eb91a980d3
kikeXD <kike@gamil.com>
2021-11-30 00:59:41 -0500

    now two players can have an endless game :D

--- game.pl
+++ game.pl
@@ -1,93 +1,95 @@
-:- module(game, [cell/6, play_now/1]).
+:- module(game, [cell/6, color_now/1, turn/1]).
 :- use_module(change_turn).
 :- use_module(bug_can_put).
 :- use_module(bug_can_move).
 :- use_module(play_can_do).
 :- use_module(possible_colocation).
 :- use_module(real_valid_moves).
+:- use_module(make_move).
 
 
-cell(queen, 0, 0, white, 0, true).
-cell(ant,  _,  _, white, -1, false).
-cell(ant,  _,  _, white, -2, false).
-cell(ant,  _,  _, white, -3, false).
-cell(ant,  _,  _, white, -4, false).
-cell(ant,  _,  _, white, -5, false).
-cell(ant,  _,  _, white, -6, false).
-cell(ant,  _,  _, white, -7, false).
-cell(ant,  _,  _, white, -8, false).
-cell(ant,  _,  _, white, -9, false).
+cell(queen,0,  0, white, -1, false).
+cell(ant,  0,  0, white, -1, false).
+cell(ant,  0,  0, white, -2, false).
+cell(ant,  0,  0, white, -3, false).
+cell(ant,  0,  0, white, -4, false).
+cell(ant,  0,  0, white, -5, false).
+cell(ant,  0,  0, white, -6, false).
+cell(ant,  0,  0, white, -7, false).
+cell(ant,  0,  0, white, -8, false).
+cell(ant,  0,  0, white, -9, false).
 
-cell(queen, _, _, black, -1, false).
-cell(ant,  _,  _, black, -1, false).
-cell(ant,  _,  _, black, -2, false).
-cell(ant,  _,  _, black, -3, false).
-cell(ant,  _,  _, black, -4, false).
-cell(ant,  _,  _, black, -5, false).
-cell(ant,  _,  _, black, -6, false).
-cell(ant,  _,  _, black, -7, false).
-cell(ant,  _,  _, black, -8, false).
-cell(ant,  _,  _, black, -9, false).
+cell(queen, 0,  0, black, -1, false).
+cell(ant,   0,  0, black, -1, false).
+cell(ant,   0,  0, black, -2, false).
+cell(ant,   0,  0, black, -3, false).
+cell(ant,   0,  0, black, -4, false).
+cell(ant,   0,  0, black, -5, false).
+cell(ant,   0,  0, black, -6, false).
+cell(ant,   0,  0, black, -7, false).
+cell(ant,   0,  0, black, -8, false).
+cell(ant,   0,  0, black, -9, false).
 
 :- dynamic cell/6.
 
-play_now(white).
+color_now(white).
 
-:- dynamic play_now/1.
+:- dynamic color_now/1.
 
 turn(1).
 
 :- dynamic turn/1.
 
 infinity_loop() :-
-    play_now(Color),
+    color_now(Color),
     turn(Turn),
+    write("Color: " + Color + " Turn: " + Turn + "\n"),
 
     play_can_do(Color, PlayCanDo),
-    write(PlayCanDo),
-    write("\n"),
+    write("Select play to do " + PlayCanDo + "\n"),
     read(PlayToDo),
     nth1(PlayToDo, PlayCanDo, Play),
-    write(Play),
-    write("\n"),
+    write("Play selected " + Play + "\n"),
 
     play(Play, Color, Turn),
+
+    change_turn(_),
+    advance_turn(),
+
+    write("----------------------------------------------------------\n"),
 
     infinity_loop().
 
 
 play(put, Color, Turn) :-
     bug_can_put(Color, Turn, BugCanPut),
-    write(BugCanPut),
-    write("\n"),
+    write("Select bug tu put: " + BugCanPut + "\n"),
     read(BugToPut),
     nth1(BugToPut, BugCanPut, Bug),
-    write(Bug),
-    write("\n"),
+    write("Bug selected: " + Bug + "\n"),
 
     possible_colocation(Color, PossiblesColocations),
-    write(PossiblesColocations),
-    write("\n"),
+    write("Select colocation: " + PossiblesColocations + "\n"),
     read(ColocationSelected),
     nth1(ColocationSelected, PossiblesColocations, Colocation),
-    write(Colocation),
-    write("\n").
-    % poner al bicho y tal
+    write("Colocation selected: " + Colocation + "\n"),
+
+    select_cell_to_put(Color, Bug, [B, R, C, Color, Sp]),
+    make_move(cell(B, R, C, Color, Sp, false), Colocation).
+
+
 
 play(move, Color, _) :-
     bug_can_move(Color, BugCanMove),
-    write(BugCanMove),
-    write("\n"),
+    write("Select bug to move: " + BugCanMove + "\n"),
     read(BugToMove),
     nth1(BugToMove, BugCanMove, [B, R, C, Color, Sp]),
-    write([B, R, C, Color, Sp]),
-    write("\n"),
+    write("Bug selected " + [B, R, C, Color, Sp] + "\n"),
 
     real_valid_moves(cell(B, R, C, Color, Sp, true), RealValidMoves),
-    write(RealValidMoves),
-    write("\n"),
+    write("Select move to do " + RealValidMoves + "\n"),
     read(MoveToDo),
     nth1(MoveToDo, RealValidMoves, Move),
-    write(Move),
-    write("\n").
-    %mover al bicho y tal
+    write("Move selected: " + Move + "\n"),
+
+    make_move(cell(B, R, C, Color, Sp, true), Move).
