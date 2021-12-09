1cb8fc8ea909ed05416ca180652ad945dcb89aff
kikeXD <kike@gamil.com>
2021-12-09 03:25:37 -0500

    fixing if in hive

--- hive.pl
+++ hive.pl
@@ -11,23 +11,31 @@ delete(Element,[Head|Tail],[Head|Tail1]) :-
 hive_consitent() :- not(cell(_, _, _, _, _, true)).
 
 hive_consitent() :-
-    cell(_, R, C, _, _, true),
-    findall([X,Y], cell(_, X, Y, _, _, true), Cells),
-    delete([R, C], Cells, Cells1),
-    hive_dfs(cell(_, R, C, _, _, true), Cells1, UntouchedCells),
+    findall([X,Y], cell(_, X, Y, _, _, true), [[R, C]| Cells]),
+    hive_dfs(cell(_, R, C, _, _, true), Cells, UntouchedCells),
     UntouchedCells = [].
     
 
 hive_dfs(cell(_, Row, Column, _, _, true), Cells, UntouchedCells) :-
     R1 is Row - 1, C1 is Column,
-    (member([R1, C1], Cells) -> (delete([R1, C1], Cells, Cells12), hive_dfs(cell(_, R1, C1, _, _, true), Cells12, Cells1)); append([Cells], Cells1)),
-    R2 is Row - 1, C2 is Column + 1, 
-    (member([R2, C2], Cells1) -> (delete([R2, C2], Cells1, Cells22), hive_dfs(cell(_, R2, C2, _, _, true), Cells22, Cells2)); append([Cells1], Cells2)),
+    hive_dfs_visit([R1, C1], Cells, Cells1),
+    R2 is Row - 1, C2 is Column + 1,
+    hive_dfs_visit([R2, C2], Cells1, Cells2),
     R3 is Row,     C3 is Column - 1,
-    (member([R3, C3], Cells2) -> (delete([R3, C3], Cells2, Cells32), hive_dfs(cell(_, R3, C3, _, _, true), Cells32, Cells3)); append([Cells2], Cells3)),
+    hive_dfs_visit([R3, C3], Cells2, Cells3),
     R4 is Row,     C4 is Column + 1,
-    (member([R4, C4], Cells3) -> (delete([R4, C4], Cells3, Cells42), hive_dfs(cell(_, R4, C4, _, _, true), Cells42, Cells4)); append([Cells3], Cells4)),
+    hive_dfs_visit([R4, C4], Cells3, Cells4),
     R5 is Row + 1, C5 is Column - 1,
-    (member([R5, C5], Cells4) -> (delete([R5, C5], Cells4, Cells52), hive_dfs(cell(_, R5, C5, _, _, true), Cells52, Cells5)); append([Cells4], Cells5)),
+    hive_dfs_visit([R5, C5], Cells4, Cells5),
     R6 is Row + 1, C6 is Column,
-    (member([R6, C6], Cells5) -> (delete([R6, C6], Cells5, Cells62), hive_dfs(cell(_, R6, C6, _, _, true), Cells62, UntouchedCells)); append([Cells5], UntouchedCells)).
+    hive_dfs_visit([R6, C6], Cells5, UntouchedCells).
+
+
+hive_dfs_visit([R, C], Cells, UntouchedCells) :-
+    member([R, C], Cells),
+    delete([R, C], Cells, D),!,
+
+    hive_dfs(cell(_, R, C, _, _, true), D, UntouchedCells).
+
+hive_dfs_visit([R, C], Cells, Cells) :-
+    not(member([R, C], Cells)).
